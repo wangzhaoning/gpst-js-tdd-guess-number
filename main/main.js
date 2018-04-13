5c5d9d0a4073ed8adf25daff71fefe09ec29114f
maskingtimeflows <1078674571@qq.com>
2018-04-13 18:29:40 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,6 +1,35 @@
 // Write your cade below:
-function main() {};
+module.exports =function main(input,random) {
+    var Anum=0;
+    var Bnum=0;
+    var num=0;
+    if(input==="")
+    {
+        return "0A0B";
+    }
+    for(let i=0;i<input.length;i++)
+    {
+        if(input.charAt(i)===random.charAt(i))
+        {
+            Anum++;
+        }
+    }
+    for(let i=0;i<input.length;i++)
+    {
+        var b=false;
+        for(let j=0;j<input.length;j++)
+        {
+            if(input.charAt(i)===random.charAt(j))
+            {
+                b=true;
+            }
+        }
+        if(b===true)
+        {
+            num++;
+        }
+    }
+    Bnum=num-Anum;
+    return Anum+"A"+Bnum+"B";
+};
 
-module.exports = {
-    main
-}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,29 @@
 const main = require('../main/main');
 
-describe('main()', () => {
-
+describe('main()', function ()
+ {
+    it("全猜对A的情况", function () {
+        let input="1234";
+        let random="1234";
+        var result = main(input,random);
+        expect(result).toEqual("4A0B");
+    })
+     it("", function () {
+         let input="1234";
+         let random="1243";
+         var result = main(input,random);
+         expect(result).toEqual("2A2B");
+     })
+     it("B全猜对的情况", function () {
+         let input="1234";
+         let random="4321";
+         var result = main(input,random);
+         expect(result).toEqual("0A4B");
+     })
+     it("为空", function () {
+         let input="";
+         let random="1234";
+         var result = main(input,random);
+         expect(result).toEqual("0A0B");
+     })
 });
